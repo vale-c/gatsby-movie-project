b5bcb9bf34ff7c79b20aff1982e4ea047dcd25b6
Vale <vale@MacBook-Pro.local>
2020-05-06 11:07:57 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Link to="/series/">Browse TV Series</Link>
+  </Layout>
+)
+
+export default IndexPage
